bc6a744e150957f0fa69654aa5f46d5f7e86765b
orangebird42 <iamsamanthaorange@gmail.com>
2023-05-08 12:01:15 -0500

    Split words success

--- VideoFinder.py
+++ VideoFinder.py
@@ -4,23 +4,34 @@ import urllib.request
 import re
 import urllib
 
+def splitSentence(sentence):
+    return re.findall(r"\b\w+\b", sentence)
+
+
 def Finder(Search):
-    
+    words = splitSentence(Search)
+    for word in words:
+
     # Properly encode search query
-    signSearch = urllib.parse.quote(Search + " asl dictionary")
-    URL = f'https://youtube.com/results?search_query={signSearch}'
+        signSearch = urllib.parse.quote(word + " asl dictionary")
+        URL = f'https://youtube.com/results?search_query={signSearch}'
 
-    html = urllib.request.urlopen(URL)
-    videoIds = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    videoURL = "https://youtube.com/watch?v=" + videoIds[0]
+        html = urllib.request.urlopen(URL)
+        videoIds = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+        videoURL = "https://youtube.com/watch?v=" + videoIds[0]
 
-    with open("URLLinks.txt", "a") as linkFile:
-        linkFile.write(videoURL + "\n")
-    linkFile.close()
+        with open("URLLinks.txt", "a") as linkFile:
+            linkFile.write(videoURL + "\n")
+        linkFile.close()
+
+    
+    
+
 
 def Clear():
     linkFile2 = open('URLLinks.txt', 'r+')
     linkFile2.truncate(0) # need '0' when using r+
+
 
 
 
