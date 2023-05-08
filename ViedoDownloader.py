4950a98acab3b11475fda6ba8d2720ba7ed26d9a
orangebird42 <iamsamanthaorange@gmail.com>
2023-05-08 14:41:36 -0500

    download 1 work

--- ViedoDownloader.py
+++ ViedoDownloader.py
@@ -1,3 +1,23 @@
 from pytube import YouTube
 
-savePath = "\AVERIS\VideoFiles"
+savePath = r"C:\Users\iamsa\Desktop\Code\AVERIS\VideoFiles"
+
+with open("URLLinks.txt", "r") as link:
+    for i in link:
+        try:
+            # Create a YouTube object
+            yt = YouTube(i)
+
+            # Filter the streams to get mp4 files
+            mp4files = yt.streams.filter(file_extension='mp4')
+
+            # Get the highest resolution video
+            d_video = mp4files.get_highest_resolution()
+
+            # Download the video
+            d_video.download(savePath)
+            print("Download completed for:", i)
+        except Exception as e:
+            print("Error occurred:", str(e))
+
+print('Task Completed!')
